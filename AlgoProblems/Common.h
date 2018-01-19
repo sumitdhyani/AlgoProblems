b39378c5a5346d3c1f879350680dad045c95a6f5
sumitdhyani <sumitdhyani@yahoo.com>
2018-01-19 15:31:00 +0530

    Added producer consumer

--- AlgoProblems/Common.h
+++ AlgoProblems/Common.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include <iostream>	
 #include <fstream>
+#include <thread>
 
 #if !defined(UINT)
 typedef unsigned int UINT;
@@ -113,3 +114,153 @@ public:
 
 typedef BinaryHeap<UINT, std::less<UINT>> MinHeap;
 typedef BinaryHeap<UINT, std::greater<UINT>> MaxHeap;
+
+
+
+class Thread
+{
+	std::thread* m_thread;
+	bool m_ThreadRunning;
+	virtual void run() = 0;
+public:
+
+	void start()
+	{
+		m_thread = new std::thread(&Thread::run,this);
+	}
+
+	std::thread* operator->()
+	{
+		if (NULL == m_thread)
+			throw std::runtime_error("Thread not started yet");
+		else
+			return m_thread;
+	}
+
+	virtual ~Thread()
+	{
+		delete m_thread;
+	}
+};
+
+template <class T>
+class SingleProducer : public Thread
+{
+	std::mutex& m_mutex;
+	std::condition_variable& m_cv;
+
+	virtual void run()
+	{
+		while (true)
+		{
+			std::unique_lock<std::mutex> lk(m_mutex);
+			bool consumerWaiting = false;
+			if (isQueueEmpty())
+				consumerWaiting = true;
+			pushItem(getNextElement());
+			if (consumerWaiting)
+			{
+				lk.unlock();
+				m_cv.notify_one();
+			}
+		}
+	}
+
+	virtual bool isQueueEmpty() = 0;
+	virtual void pushItem(T queueItem) = 0;
+	virtual T getNextElement() = 0;
+public:
+	SingleProducer(std::mutex& mutex, std::condition_variable& cv)
+		:m_mutex(mutex),
+		m_cv(cv)
+	{
+	}
+};
+
+template <class T>
+class SingleConsumer : public Thread
+{
+	std::mutex& m_mutex;
+	std::condition_variable& m_cv;
+	virtual void run()
+	{
+		while (true)
+		{
+			std::unique_lock<std::mutex> lk(m_mutex);
+			if (isQueueEmpty())
+				m_cv.wait(lk);
+
+			while (!isQueueEmpty())
+				onNewItem(pullItem());
+		}
+	}
+
+	virtual bool isQueueEmpty() = 0;
+	virtual T pullItem() = 0;
+	virtual void onNewItem(T item) = 0;
+public:
+	SingleConsumer(std::mutex& mutex, std::condition_variable& cv)
+		:m_mutex(mutex),
+		 m_cv(cv)
+	{
+	}
+};
+
+
+template <class T>
+class FifoQueueProducer : public SingleProducer < T >
+{
+	std::vector<T>& m_eventQueque;
+	virtual bool isQueueEmpty()
+	{
+		return m_eventQueque.empty();
+	}
+
+	void pushItem(T queueItem)
+	{
+		m_eventQueque.push_back(queueItem);
+	}
+
+	virtual T pullItem()
+	{
+		T item = m_eventQueque.back();
+		m_eventQueque.pop_back();
+		return item;
+	}
+
+public:
+	FifoQueueProducer(std::vector<T>& queue, std::mutex& mutex, std::condition_variable& cv)
+		:SingleProducer < T >(mutex,cv),
+		m_eventQueque(queue)
+	{
+	}
+};
+
+template <class T>
+class FifoQueueConsumer : public SingleConsumer < T >
+{
+	std::vector<T>& m_eventQueque;
+	virtual bool isQueueEmpty()
+	{
+		return m_eventQueque.empty();
+	}
+
+	virtual T pullItem()
+	{
+		if (!m_eventQueque.empty())
+		{
+			T item = m_eventQueque.back();
+			m_eventQueque.pop_back();
+			return item;
+		}
+		else
+			throw std::runtime_error("The event queue is empty");
+
+	}
+public:
+	FifoQueueConsumer(std::vector<T>& queue, std::mutex& mutex, std::condition_variable& cv)
+		:SingleConsumer < T >(mutex, cv),
+		m_eventQueque(queue)
+	{
+	}
+};
